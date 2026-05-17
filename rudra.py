0fc36d05a3695953c5d38c0813f2470ad21ff7f5
rudra <rudraptl11903@gmail.com>
2026-05-17 21:47:02 +0530

    First commit

---
+++ rudra.py
@@ -0,0 +1,32 @@
+# variables and data types
+a = 10          # int
+b = 3.14        # float
+c = "Python"    # string
+d = True        # boolean
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#oprators
+x = 5   
+y = 2
+print("Addition:", x + y)        # Addition
+print("Subtraction:", x - y)     # Subtraction
+print("Multiplication:", x * y)  # Multiplication
+print("Division:", x / y)        # Division
+print("Floor Division:", x // y) # Floor Division
+print("Modulus:", x % y)         # Modulus  
+
+
+#print & input 
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+next_age = age + 1
+
+print("Name =", name)
+print("Current Age =", age)
+print("Next Year Age =", next_age)
