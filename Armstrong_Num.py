72240a7c10c964578269c5b10512d0b555ddab8f
rudraptl11903 <rudraptl11903@gmail.com>
2026-05-20 21:55:53 +0530

    Add files via upload

---
+++ Armstrong_Num.py
@@ -0,0 +1,20 @@
+# Program to check Armstrong number
+
+num = int(input("Enter a number: "))
+
+order = len(str(num))
+
+sum = 0
+temp = num
+
+# Calculate sum of digits raised to power
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+
+# Check Armstrong condition
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
