589077f5754815e0dffbaac62a857e46c7cff82c
rudra <rudraptl11903@gmail.com>
2026-05-25 22:31:10 +0530

    ----

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#tuple basic
+
+#using String
+tup = ("rudra" , "patel")
+print(tup)
+
+# Using List
+li = [1,2,3,4,5]
+print(tuple(li))
+
+# Using Built-in Function
+tup = tuple('rudra')
+print(tup)
+
