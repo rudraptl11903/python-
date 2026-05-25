de680de336f9f09a1d3171fc7fe37913ec8cded8
rudra <rudraptl11903@gmail.com>
2026-05-25 22:35:29 +0530

    ----

--- tuple.py
+++ tuple.py
@@ -12,3 +12,12 @@ print(tuple(li))
 tup = tuple('rudra')
 print(tup)
 
+#Creating a Tuple with Mixed Datatypes
+tup = (5, 'Welcome', 7.5, True, [1, 2, 3], {'key': 'value'})
+print(tup)
+
+
+tup = tuple('Rudrapatel')
+print(tup[1:])
+print(tup[::-1])
+print(tup[4:9])
