472b8b58978e9cd036645674208bee6d7586d678
rudraptl11903 <rudraptl11903@gmail.com>
2026-06-02 22:11:54 +0530

    added
    Added a nested class and corrected instance attribute initialization.

--- oop.py
+++ oop.py
@@ -6,4 +6,14 @@ class Dog:
         self.name = name 
         self.age = age  
 
-    
+#Creating Object
+    class dog:
+        r = "canine "
+    def __init__(self, name, age):
+        self.name = name  # Instance attribute
+        self.age = age  # Instance attribute
+
+# Creating an object of the Dog class
+dog1 = Dog("Buddy", 3)
+print(dog1.name) 
+print(dog1.species)
